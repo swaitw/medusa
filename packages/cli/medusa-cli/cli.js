0a95c6f6df2280b00b755a3a638dc9ba86ceb960
Adrien de Peretti <adrien.deperetti@gmail.com>
2025-02-19 20:02:47 +0100

    chore(cli): Prevent swallowing error in non production env (#11534)

--- packages/cli/medusa-cli/cli.js
+++ packages/cli/medusa-cli/cli.js
@@ -3,6 +3,14 @@
 try {
   require("ts-node").register({})
   require("tsconfig-paths").register({})
-} catch {}
+} catch (e) {
+  const isProduction = process.env.NODE_ENV === "production"
+  if (!isProduction) {
+    console.warn(
+      "ts-node cannot be loaded and used, if you are running in production don't forget to set your NODE_ENV to production"
+    )
+    console.warn(e)
+  }
+}
 require("dotenv").config()
 require("./dist/index.js")
