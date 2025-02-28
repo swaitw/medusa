de0798dbed29bc37963d72001a02be7b69e5e88b
Frane Polić <16856471+fPolic@users.noreply.github.com>
2025-02-28 10:12:45 +0100

    fix(dashboard): display CG in PL configuration details (#11653)
    CLOSES CMRC-966

--- packages/admin/dashboard/src/routes/price-lists/price-list-detail/components/price-list-configuration-section/price-list-configuration-section.tsx
+++ packages/admin/dashboard/src/routes/price-lists/price-list-detail/components/price-list-configuration-section/price-list-configuration-section.tsx
@@ -55,7 +55,7 @@ const CustomerGroupDisplay = ({
 }) => {
   const { t } = useTranslation()
 
-  const customerGroupIds = priceList.rules.customer_group_id as
+  const customerGroupIds = priceList.rules["customer.groups.id"] as
     | string[]
     | undefined
 
